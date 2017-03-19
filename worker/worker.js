792692d50b903adee38b01ea3ff8306ae6f78374
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2017-03-19 18:46:40 +0100

    Initial setup node worker

---
+++ worker/worker.js
@@ -0,0 +1,42 @@
+var amqp = require('amqplib/callback_api');
+var async = require('async');
+
+var statusExchange = 'rpc_status';
+var service = 'opencpu';
+
+function handleRequest(ch) {
+  return function(msg) {
+    var str = msg.content.toString();
+    var taskId = msg.properties.correlationId;
+    var replyTo = msg.properties.replyTo;
+    ch.publish('', replyTo, new Buffer(JSON.stringify({ "message": "Hello world"})), { contentType: 'application/json' });
+    ch.ack(msg);
+  };
+}
+
+async.waterfall([
+  function(callback) {
+    amqp.connect('amqp://' + process.env.PATAVI_BROKER_HOST, callback);
+  },
+  function(conn, callback) {
+    conn.createChannel(callback);
+  },
+  function(ch, callback) {
+    ch.assertExchange(statusExchange, 'topic', { durable: false }, function(err) {
+      callback(err, ch);
+    });
+  },
+  function(ch, callback) {
+    ch.assertQueue(service, { exclusive: false, durable: true }, function(err) {
+      callback(err, ch);
+    });
+  },
+  function(ch, callback) {
+    ch.consume(service, handleRequest(ch), { noAck: false }, callback);
+  }
+], function(err) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+});
